f9a7b2d74b6b6b8686aa20f3e139430e2696681d
gaofengoo <15879831437@163.com>
2020-06-11 17:09:43 +0800

    update

---
+++ my-project/src/components/form.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="hello">
+		<form novalidate>
+			<div>
+				<label for="username">username:</label>
+				<input type="text" name="" id="username">
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'validator',
+	props: {
+		msg: String
+	},
+	data() {
+		return {
+			
+		};
+	}
+}
+</script>
