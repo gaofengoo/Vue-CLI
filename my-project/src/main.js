3dda6f8e1a3ddb9f5627ca7d5dc14d285a7ca8be
gaofengoo <15879831437@163.com>
2020-06-11 18:16:01 +0800

    update

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import VueValidator from 'vue-validator';
-// import VueResource from 'vue-resource';
+import VueResource from 'vue-resource';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 
 // Vue.use(ElementUI, {size:'small', zIndex:'3000'});
 Vue.use(ElementUI);
 Vue.use(VueValidator);
-// Vue.use(VueResource);
+Vue.use(VueResource);
 
 Vue.config.productionTip = false;
 
